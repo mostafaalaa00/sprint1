b26f6a6ff1f17f87f262083b123498599242ddd1
mostafaalaa00 <61460174+mostafaalaa00@users.noreply.github.com>
2020-04-03 18:47:00 +0200

    Update IUserRepository.java

--- src/main/java/com/phase1/repository/IUserRepository.java
+++ src/main/java/com/phase1/repository/IUserRepository.java
@@ -1,11 +1,15 @@
 package com.phase1.repository;
 import org.springframework.data.repository.CrudRepository;
-import com.phase1.models.User;
+
+import com.phase1.models.Buyers;
+//import com.phase1.models.Admin;
+//import com.phase1.models.User;
+
 
 // This will be AUTO IMPLEMENTED by Spring into a Bean called userRepository
 // CRUD refers Create, Read, Update, Delete
 
-public interface IUserRepository extends CrudRepository<User, Long>
+public interface IUserRepository extends CrudRepository<Buyers, Long>
 {
 
 }
